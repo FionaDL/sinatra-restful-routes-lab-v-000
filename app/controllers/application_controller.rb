6e59fce6435bb46e265088d8349d85d3b2b8be71
Fiona Lapham <fionadlapham@gmail.com>
2018-07-13 10:11:17 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,13 +10,6 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
-  post '/recipes' do
-    params
-    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time] )
-    @recipe.save
-    redirect to '/recipe/#{@recipe.id}'
-  end
-
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
@@ -25,6 +18,13 @@ class ApplicationController < Sinatra::Base
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
+  end
+
+  post '/recipes' do
+    params
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time] )
+    @recipe.save
+    redirect to '/recipe/#{@recipe.id}'
   end
 
 end
