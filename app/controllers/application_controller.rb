8d5f52f56bfde350c49655a9e9e5165af0bfaa31
Fiona Lapham <fionadlapham@gmail.com>
2018-07-13 11:22:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,11 @@
 require './config/environment'
-require 'pry'
 
 class ApplicationController < Sinatra::Base
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+  # register Sinatra::ActiveRecordExtension
+  set :views, Proc.new { File.join(root, "../views/") }
 
-  get '/recipes/new' do
+  get '/recipes/new' do 
     erb :new
-  end
-
-  post '/recipes' do
-    @recipe = Recipe.create(params)
-    redirect to "/recipes/#{@recipe.id}"
-  end
-
-  get '/recipes/:id' do
-    @recipe = Recipe.find_by_id(params[:id])
-    erb :show
-  end
-
-  get 'recipes/:id/edit' do
-    @recipe = Recipe.find_by_id(params[:id])
-    binding.pry
-    erb :edit
   end
 
   get '/recipes' do
@@ -32,7 +13,17 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  patch '/recipes/:id' do
+  get '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do  
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
@@ -41,10 +32,17 @@ class ApplicationController < Sinatra::Base
     redirect to "/recipes/#{@recipe.id}"
   end
 
-  delete '/recipes/<%=@recipe.id%>/delete' do
-    @recipe.clear
+  post '/recipes' do 
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id/delete' do #delete action
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
     redirect to '/recipes'
   end
 
 
 end
+
